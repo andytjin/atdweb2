9a8be1cd3fc5083b1a292046243b104270459406
Max <Max@192.168.56.1>
2015-06-17 17:37:13 +0200

    merge tussen max en andy's code

--- src/main/Java/Servlets/KlantenOverzichtServlet.java
+++ src/main/Java/Servlets/KlantenOverzichtServlet.java
@@ -8,11 +8,22 @@ import Service.KlantService;
 import Service.ServiceProvider;
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.mail.*;
 
 /**
  *
@@ -33,37 +44,73 @@ public class KlantenOverzichtServlet extends HttpServlet {
         String button = request.getParameter("button");
 
         if (button.equals("Zoek klant")) {
-            //ga klant zoeken
+            KlantService ks = ServiceProvider.getKlantService();
+            Klant k = ks.getKlant(uname);
+
         } else {
             KlantService ks = ServiceProvider.getKlantService();
             List<Klant> lijst = ks.getAlleKlanten();
             for (Klant k : lijst) {
                 if (k.isWiltHerinnering()) {
-                    String emailadres = "maxraadgever@gmail.com";
-                    
-                    String myPassword = "password";
-                    String senderId = "xyz@yahoo.com";
+                    Properties props = System.getProperties();
+                    props.put("mail.smtp.starttls.enable", true); // added this line
+                    props.put("mail.smtp.host", "smtp.gmail.com");
+                    props.put("mail.smtp.user", "username");
+                    props.put("mail.smtp.password", "password");
+                    props.put("mail.smtp.port", "587");
+                    props.put("mail.smtp.auth", true);
+
+                    Session session = Session.getInstance(props, null);
+                    MimeMessage message = new MimeMessage(session);
+
+                    System.out.println("Port: " + session.getProperty("mail.smtp.port"));
+
+                    // Create the email addresses involved
                     try {
-                        MultiPartEmail email = new MultiPartEmail();
-                        email.setSmtpPort(587);
-                        //email.setAuthenticator(new DefaultAuthenticator(emailadres, myPassword));
-                        email.setDebug(true);
-                        email.setHostName("smtp.gmail.com");
-                        email.setFrom(emailadres);
-                        email.setSubject("Hi");
-                        email.setMsg("This is a test mail ... :-)\n\nPlease check attachements that I have sent.\n\nThanks,\nFahim");
-                        email.addTo(senderId);
-                        email.setTLS(true);
-                        
-                        email.send();
-                        System.out.println("Mail sent!");
-                    } catch (Exception e) {
-                        System.out.println("Exception :: " + e);
+                        InternetAddress from = new InternetAddress("to4henkpaladijn@gmail.com");
+                        message.setSubject("Yes we can");
+                        message.setFrom(from);
+                        message.addRecipients(Message.RecipientType.TO, InternetAddress.parse("maxraadgever@gmail.com"));
+
+                        // Create a multi-part to combine the parts
+                        Multipart multipart = new MimeMultipart("alternative");
+
+                        // Create your text message part
+                        BodyPart messageBodyPart = new MimeBodyPart();
+                        messageBodyPart.setText("some text to send");
+
+                        // Add the text part to the multipart
+                        multipart.addBodyPart(messageBodyPart);
+
+                        // Create the html part
+                        messageBodyPart = new MimeBodyPart();
+                        String htmlMessage = "Our html text";
+                        messageBodyPart.setContent(htmlMessage, "text/html");
+
+                        // Add html part to multi part
+                        multipart.addBodyPart(messageBodyPart);
+
+                        // Associate multi-part with message
+                        message.setContent(multipart);
+
+                        // Send message
+                        Transport transport = session.getTransport("smtp");
+                        transport.connect("smtp.gmail.com", "to4henkpaladijn", "themaopdracht4");
+                        System.out.println("Transport: " + transport.toString());
+                        transport.sendMessage(message, message.getAllRecipients());
+
+                    } catch (AddressException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (MessagingException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
                     }
                 }
             }
 
         }
+        request.getRequestDispatcher("KlantenOverzicht.jsp").forward(request, response);
 
     }
 
