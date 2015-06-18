95816e961165f6565d46f90b8ff3f7c73b7c09c8
Max <Max@192.168.56.1>
2015-06-18 10:56:56 +0200

    TestPackage aangemaakt

---
+++ src/test/java/BlackBoxTests/test.java
@@ -0,0 +1,42 @@
+/*
+ * Licsense Header
+ */
+package BlackBoxTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Max
+ */
+public class test {
+    
+    public test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void Test01(){
+        
+    }
+}
